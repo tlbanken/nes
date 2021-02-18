034dddb1bf2980c37eeca43988565932f2988a7a
tlbanken <bankenlt63@gmail.com>
2021-02-17 17:14:29 -0700

    Move build system to cmake and bundle sdl2 into project

---
+++ build/CMakeFiles/nes.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/travis/Projects/nes/src/apu.c" "/home/travis/Projects/nes/build/CMakeFiles/nes.dir/src/apu.c.o"
+  "/home/travis/Projects/nes/src/cart.c" "/home/travis/Projects/nes/build/CMakeFiles/nes.dir/src/cart.c.o"
+  "/home/travis/Projects/nes/src/cpu.c" "/home/travis/Projects/nes/build/CMakeFiles/nes.dir/src/cpu.c.o"
+  "/home/travis/Projects/nes/src/mappers/map000.c" "/home/travis/Projects/nes/build/CMakeFiles/nes.dir/src/mappers/map000.c.o"
+  "/home/travis/Projects/nes/src/mappers/map001.c" "/home/travis/Projects/nes/build/CMakeFiles/nes.dir/src/mappers/map001.c.o"
+  "/home/travis/Projects/nes/src/mappers/map002.c" "/home/travis/Projects/nes/build/CMakeFiles/nes.dir/src/mappers/map002.c.o"
+  "/home/travis/Projects/nes/src/mem.c" "/home/travis/Projects/nes/build/CMakeFiles/nes.dir/src/mem.c.o"
+  "/home/travis/Projects/nes/src/nes.c" "/home/travis/Projects/nes/build/CMakeFiles/nes.dir/src/nes.c.o"
+  "/home/travis/Projects/nes/src/ppu.c" "/home/travis/Projects/nes/build/CMakeFiles/nes.dir/src/ppu.c.o"
+  "/home/travis/Projects/nes/src/utils.c" "/home/travis/Projects/nes/build/CMakeFiles/nes.dir/src/utils.c.o"
+  "/home/travis/Projects/nes/src/vac.c" "/home/travis/Projects/nes/build/CMakeFiles/nes.dir/src/vac.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../extern/include"
+  "../extern/SDL/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/travis/Projects/nes/build/extern/SDL/CMakeFiles/SDL2-static.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/nes.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nes.dir/src/apu.c.o"
+  "CMakeFiles/nes.dir/src/cart.c.o"
+  "CMakeFiles/nes.dir/src/cpu.c.o"
+  "CMakeFiles/nes.dir/src/mappers/map000.c.o"
+  "CMakeFiles/nes.dir/src/mappers/map001.c.o"
+  "CMakeFiles/nes.dir/src/mappers/map002.c.o"
+  "CMakeFiles/nes.dir/src/mem.c.o"
+  "CMakeFiles/nes.dir/src/nes.c.o"
+  "CMakeFiles/nes.dir/src/ppu.c.o"
+  "CMakeFiles/nes.dir/src/utils.c.o"
+  "CMakeFiles/nes.dir/src/vac.c.o"
+  "nes"
+  "nes.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/nes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
